afd0f2760bbe9ae16147bf5e3a8689654c252fef
Thomas Watson <Watson-T4@ulster.ac.uk>
2020-05-06 16:48:43 +0100

    Added Extent Reporting.

--- SetupEnvironment/BaseTest.cs
+++ SetupEnvironment/BaseTest.cs
@@ -1,12 +1,20 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using OpenQA.Selenium.Chrome;
 
 namespace SetupEnvironment
 {
+    [SetUpFixture]
     public class BaseTest
     {
         public static IWebDriver driver = new ChromeDriver();
+        public static new ExtentTest ReporterTest;
+        public static new ExtentHtmlReporter htmlReporter;
+        public static new ExtentReports extent;
+        public static new Exception LastException { get; private set; }
 
         static void Main()
         {
@@ -16,13 +24,21 @@ namespace SetupEnvironment
         [OneTimeSetUp]
         public void Initialize()
         {
-            //Something
+            ReporterTest.Log(Status.Info, $"|**|Nunit is initializing extent reports for test runs.|**|");
+            extent = new ExtentReports();
         }
 
         [OneTimeTearDown]
         public void CleanUp()
         {
-           driver.Quit();
+            htmlReporter = new ExtentHtmlReporter("C:\\Reports\\TestRun.html");
+
+            extent.AttachReporter(htmlReporter);
+            ReporterTest.Log(Status.Info, $"|**|Nunit has attached extent report.|**|");
+            
+            extent.Flush();
+            driver.Quit();
         }
+
     }
 }
--- SetupEnvironment/EntryPoint.cs
+++ SetupEnvironment/EntryPoint.cs
@@ -1,18 +1,51 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using NLog;
+using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Net;
 
 namespace SetupEnvironment
 {
     [Parallelizable]
     public class EntryPoint
     {
-       [Test]
+
+        [SetUp]
+        public void TestSetup()
+        {
+            BaseTest.ReporterTest = BaseTest.extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            BaseTest.ReporterTest.Log(Status.Info, $"|**|Nunit Test [{TestContext.CurrentContext.Test.Name}] STARTED |**|");
+        }
+
+        [Test]
         [Category("Parallel Search Engine Tests")]
         public void GoogleTest1()
         {
             var url = "www.google.co.uk";
+        }
 
-            BaseTest.driver.Navigate().GoToUrl(url);
+        [TearDown]
+        public void WebFixtureCleanup()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status.ToString().Equals("Failed"))
+            {
+                try
+                {
+                    BaseTest.ReporterTest.Fail(WebUtility.HtmlEncode(BaseTest.LastException.Message.ToString()));
+                }
+                catch (Exception exception)
+                {
+                    BaseTest.ReporterTest.Fail(WebUtility.HtmlEncode(exception.ToString()));
+                }
+            }
+            else
+            {
+                BaseTest.ReporterTest.Pass(TestContext.CurrentContext.Test.Name);
+            }
+
+            BaseTest.ReporterTest.Log(Status.Info, $"|**| Nunit Test [{TestContext.CurrentContext.Test.Name}] COMPLETED |**|");
         }
     }
 }
